3f29323821c233d7f7d3b2e870368c61822de31a
amilashanaka <dsa.amilashanaka@gmail.com>
2022-12-07 10:13:31 +0000

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gloversure/2022-php-challenge',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a8f310e69ce5ee8f9c82a526d7cd62dfef833cb2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gloversure/2022-php-challenge' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a8f310e69ce5ee8f9c82a526d7cd62dfef833cb2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
